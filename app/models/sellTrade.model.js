7cf1c60bc6a148aa02728299faa03c495f394a8b
Pallishree <pallishreebehera01@gmail.com>
2024-05-22 16:51:31 +0530

    small change in model

--- app/models/sellTrade.model.js
+++ app/models/sellTrade.model.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 
-const sellTradeSchema=new mongoose.Schema({
-    pickuplocation:{type:String},
-    category:{type:String, },
-    product:{type:String},
-    variety:{type:String},
-    grade:{type:String},
-    username:{type:String},
-    price:{type:Number,default:0},
-    quantity:{type:String,default:0},
-    priceQuantityUnit:{type:String},
-    Date:{type:String},
+const sellTradeSchema = new mongoose.Schema({
+    pickuplocation: { type: String },
+    category: { type: String },
+    product: { type: String },
+    variety: { type: String },
+    grade: { type: String },
+    username: { type: String },
+    price: { type: Number, default: 0 },
+    quantity: { type: String, default: 0 },
+    priceQuantityUnit: { type: String },
+    Date: { type: String },
     image: [],
-    availableFromDate: { type:String }, // Field for the start date of availability
-    availableToDate: { type:String },   // Field for the end date of availability
-    farmer_id:{type:mongoose.Schema.Types.ObjectId,ref:"admin"}
-    
-}, { timestamps: true })
+    availableFromDate: { type: String },
+    availableToDate: { type: String },
+    farmer_id: { type: mongoose.Schema.Types.ObjectId, ref: "admin" }
+}, { timestamps: true });
 
-const sellTrade=mongoose.model("sellTrade",sellTradeSchema);
+const SellModel = mongoose.model("sellTrade", sellTradeSchema);
 
-module.exports = sellTrade
+module.exports = SellModel;
